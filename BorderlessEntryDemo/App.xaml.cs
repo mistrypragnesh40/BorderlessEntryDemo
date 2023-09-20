9358bba0837c31edff76f40da8ed3d91535b0036
Pragnesh Mistry <47309472+mistrypragnesh40@users.noreply.github.com>
2023-09-20 10:31:28 +0530

    Add project files.

---
+++ BorderlessEntryDemo/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using BorderlessEntryDemo.Handler;
+
+namespace BorderlessEntryDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(CustomEntry), (handler, view) =>
+            {
+                if (view is CustomEntry)
+                {
+#if ANDROID
+                handler.PlatformView.Background = null;
+                handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+#elif IOS
+            handler.PlatformView.BackgroundColor = UIKit.UIColor.Clear;
+            handler.PlatformView.Layer.BorderWidth = 0;
+            handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+#endif
+                }
+            });
+
+
+            MainPage = new AppShell();
+        }
+    }
+}
